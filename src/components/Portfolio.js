4b286351b7526d60887b29ebbed0df10714972fd
CheBar1 <chebarclay@live.com.au>
2023-02-06 22:31:43 +1030

    added project images, updated Portfolio.js and Project.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,32 +1,71 @@
 import React from "react";
 import Project from "./Project";
 
-const Portfolio = () => {
-  const projects = [
-    {
-      name: "Weather Dashboard",
-      description: "Javascript, API, Bootstrap",
-      deployedLink: "https://writteninnacode.github.io/Weather_Dashboard/",
-      githubLink: "https://github.com/WrittenInnaCode/Weather_Dashboard",
-      image: "04-weather-dashboard.png",
-    },
-    {
-      name: "Weather Dashboard",
-      description: "Javascript, API, Bootstrap",
-      deployedLink: "https://writteninnacode.github.io/Weather_Dashboard/",
-      githubLink: "https://github.com/WrittenInnaCode/Weather_Dashboard",
-      image: "04-weather-dashboard.png",
-    },
-  ];
+import textEditor from "./../assets/images/textEditor.png";
+import socialNetworkApi from "./../assets/images/socialNetworkApi.png";
+import techBlog from "./../assets/images/techBlog.png";
+import employeeTracker from "./../assets/images/employeeTracker.png";
+import teamProfileGenerator from "./../assets/images/teamProfileGenerator.png";
+import workDayScheduler from "./../assets/images/workDayScheduler.png";
 
+const projects = [
+  {
+    title: "Text Editor",
+    description:
+      "Technologies: JavaScript, Node.js, Express.js, Webpack, Babel, IndexedDB/IDB",
+    deployedLink: "https://mysterious-reef-06963.herokuapp.com/",
+    githubLink: "https://github.com/CheBar1/text-editor",
+    image: textEditor,
+  },
+  {
+    title: "Social Network API",
+    description:
+      "Technologies: JavaScript, Node.js, Express.js, MongoDB, Mongoose, Moment",
+    deployedLink:
+      "https://drive.google.com/file/d/1QFEJ4rg9yn1scgb3ct0I9HJVaRfbJNXd/view",
+    githubLink: "https://github.com/CheBar1/social-network-api",
+    image: socialNetworkApi,
+  },
+  {
+    title: "Tech Blog",
+    description: "Technologies: MySQL, Handlebars, MVC paradigm, RESTful API",
+    deployedLink: "https://young-savannah-76133.herokuapp.com/",
+    githubLink: "https://github.com/CheBar1/tech-blog2",
+    image: techBlog,
+  },
+  {
+    title: "Employee Tracker",
+    description: "Technologies: JavaScript, jQuery, Inquirer, MySQL2",
+    deployedLink:
+      "https://drive.google.com/file/d/1xLGc6egHD_B5Nn3UnN0sFkv9CnrkGAt-/view",
+    githubLink: "https://github.com/CheBar1/employee-tracker",
+    image: { employeeTracker },
+  },
+  {
+    title: "Team Profile Generator",
+    description: "Technologies: JavaScript, Node.js, jQuery, Bootstrap, Jest",
+    deployedLink:
+      "https://drive.google.com/file/d/1bx21w2RdMyMCzXP6qdD0I2IyvNGO7VdK/view",
+    githubLink: "https://github.com/CheBar1/team-profile-generator",
+    image: teamProfileGenerator,
+  },
+  {
+    title: "Work Day Scheduler",
+    description: "Technologies: JavaScript, jQuery, HTML, CSS, Moment",
+    deployedLink: "https://chebar1.github.io/work-day-scheduler/",
+    githubLink: "https://github.com/CheBar1/work-day-scheduler",
+    image: workDayScheduler,
+  },
+];
+
+function Portfolio() {
   return (
-    <div>
+    <section>
       <h2>Portfolio</h2>
-
-      <div>
+      <div className="workContainer">
         {projects.map((project) => (
           <Project
-            name={project.name}
+            title={project.title}
             description={project.description}
             deployedLink={project.deployedLink}
             githubLink={project.githubLink}
@@ -34,8 +73,8 @@ const Portfolio = () => {
           />
         ))}
       </div>
-    </div>
+    </section>
   );
-};
+}
 
 export default Portfolio;
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,20 +1,12 @@
-// For each project you feature in your portfolio, you should include the following:
-// An image of the deployed application (either a GIF or a screenshot)
-// The title of the project
-// A link to the deployed application
-// A link to the corresponding GitHub repository
-
 import React from "react";
 
-const Project = ({ project }) => {
-  const { name, description, deployedLink, githubLink, image } = project;
-
+function Project({ title, description, deployedLink, githubLink, image }) {
   return (
     <div className="card m-3">
-      <h5 className="card-header">{name}</h5>
+      <h5 className="card-header">{title}</h5>
 
       <div className="card-body">
-        <img src={image} className="card-img-top" alt={name} />
+        <img src={image} className="card-img-top" alt={title} />
       </div>
 
       <div className="footer mx-auto mb-4">
@@ -38,6 +30,6 @@ const Project = ({ project }) => {
       </div>
     </div>
   );
-};
+}
 
 export default Project;
