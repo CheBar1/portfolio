e87edce0f759a884c74cc1f4d0f857490bcdaf6d
CheBar1 <chebarclay@live.com.au>
2023-02-06 22:33:50 +1030

    deleted non-required index.js folder from images folder

--- src/assets/images/index.js
+++
@@ -1,6 +0,0 @@
-export textEditor from "";
-export socialNetworkApi from "../../assets/images/socialNetworkApi.png";
-export techBlog from "./../assets/images/techBlog.png";
-export employeeTracker from "./../assets/images/employeeTracker.png";
-export teamProfileGenerator from "./../assets/images/teamProfileGenerator.png";
-export workDayScheduler from "./../assets/images/workDayScheduler.png";
