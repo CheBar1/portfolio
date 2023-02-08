f64049bd1ffa15687f3e4b097af30dd3d96dafe5
CheBar1 <chebarclay@live.com.au>
2023-02-08 15:02:26 +1030

    added Contact.js code and updated Footer.js with instagram logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,3 @@
-// WHEN I view the footer
-// THEN I am presented with text or icon links to the developer’s GitHub and LinkedIn profiles, and their profile on a third platform (Stack Overflow, Twitter) 
-
 import React from 'react'
 // Use rafce as shortcut to create React arrow export function
 
@@ -9,7 +6,7 @@ const Footer = () => {
     <footer className="footer bg-dark fixed-bottom">
     <a href="https://github.com/CheBar1"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="icon"/></a>
     <a href="https://www.linkedin.com/in/cheryl-barclay-531985252/"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
-    <a href="https://www.instagram.com/cheryl.k.barclay/"><img src="https://img.icons8.com/ios-glyphs/48/ffffff/trilby.png" alt="Instagram" className="icon"/></a>
+    <a href="https://www.instagram.com/cheryl.k.barclay/"><img src="https://img.icons8.com/fluency/48/null/instagram-new.png" alt="Instagram" className="icon"/></a>
 </footer>
   )
 }
